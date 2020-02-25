436109c1a959c2444b76d92b875dc77fe76cb6dc
Fonkam Loïc <fonkamloic@gmail.com>
2020-02-25 03:42:25 +0100

    platform_widget

---
+++ time_tracker_flutter_course/lib/common_widgets/platform_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
+
+
+abstract class PlatformWidget extends StatelessWidget {
+  Widget buildCupertinoWidget(BuildContext context);
+  Widget buildMaterialWidget(BuildContext context);
+
+  @override
+  Widget build(BuildContext context) {
+    if(Platform.isIOS){
+      return buildCupertinoWidget(context);
+    }
+    return buildMaterialWidget(context);
+  }
+}
