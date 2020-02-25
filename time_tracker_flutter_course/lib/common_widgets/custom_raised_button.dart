6852f896df89f488a518dbcb062cf97ff22bbe63
Fonkam Loïc <fonkamloic@gmail.com>
2020-02-25 03:40:47 +0100

    custom_raised_button

---
+++ time_tracker_flutter_course/lib/common_widgets/custom_raised_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomRaisedButton extends StatelessWidget {
+  final Widget child;
+  final VoidCallback onPressed;
+  final Color color;
+  final double borderRadius;
+  final double height;
+
+  CustomRaisedButton(
+      {this.height: 40,
+      this.child,
+      this.onPressed,
+      this.color,
+      this.borderRadius: 2.0}): assert(borderRadius != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      child: RaisedButton(
+        onPressed: onPressed,
+        elevation: 2.0,
+        color: color,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(borderRadius))),
+        child: child,
+      ),
+    );
+  }
+}
