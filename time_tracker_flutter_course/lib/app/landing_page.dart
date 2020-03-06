254c4f8965cec07c98e06643681620e1806ba11e
Fonkam Loïc <fonkamloic@gmail.com>
2020-03-06 23:50:22 +0100

    landing_page

--- time_tracker_flutter_course/lib/app/landing_page.dart
+++ time_tracker_flutter_course/lib/app/landing_page.dart
@@ -6,26 +6,25 @@ import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
 
 class LandingPage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    final auth = Provider.of<AuthBase>(context);
+    final auth = Provider.of<AuthBase>(context, listen: false);
     return StreamBuilder<User>(
       stream: auth.onAuthStateChanged,
-
-      builder: (BuildContext context, AsyncSnapshot snapshot){
-        if(snapshot.connectionState == ConnectionState.active){
-          User user =  snapshot.data;
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          User user = snapshot.data;
           if (user == null) {
-            return SignInPage(
-            );
+            return SignInPage.create(context);
           }
-          return HomePage(
-          );
+          return HomePage();
         }
-        return Scaffold(body: Center(child: CircularProgressIndicator()));
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
       },
     );
-
-}}
+  }
+}
