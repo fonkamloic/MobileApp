52dc6bd658ffb55dd82a92b8b369186fa58fe935
Fonkam Loïc <fonkamloic@gmail.com>
2020-03-11 18:32:42 +0100

    jobs page

---
+++ time_tracker_flutter_course/lib/app/home/jobs/jobs_page.dart
@@ -0,0 +1,91 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/home/job_entries/job_entries_page.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/edit_job_page.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/job_list_tile.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/list_items_builder.dart';
+import 'package:time_tracker_flutter_course/app/home/models/jobs.dart';
+import 'package:time_tracker_flutter_course/common_widgets/platform_alert_dialog.dart';
+import 'package:time_tracker_flutter_course/common_widgets/platform_exception_alert_dialog.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+import 'package:time_tracker_flutter_course/services/database.dart';
+
+class JobsPage extends StatelessWidget {
+  Future<void> _signOut(BuildContext context) async {
+    try {
+      final auth = Provider.of<AuthBase>(context, listen: false);
+      await auth.signOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future<void> _confirmSignOut(BuildContext context) async {
+    final didRequestSignOut = await PlatformAlertDialog(
+      title: 'Logout',
+      content: 'Are you sure you want to Logout?',
+      cancelActionText: 'Cancel',
+      defaultActionText: 'Logout',
+    ).show(context);
+    if (didRequestSignOut == true) {
+      _signOut(context);
+    }
+  }
+Future<void> _delete(BuildContext context, Job job) async {
+    try {
+      final database = Provider.of<Database>(context, listen: false);
+      await database.deleteJob(job);
+    } on PlatformException catch (e) {
+      PlatformExceptionAlertDialog(title: "Operation failed", exception: e)
+          .show(context);
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Jobs'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              "Logout",
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+            onPressed: () => _confirmSignOut(context),
+          )
+        ],
+      ),
+      body: _buildContents(context),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => EditJobPage.show(context, database: Provider.of<Database>(context, listen:  false),),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  
+
+  Widget _buildContents(BuildContext context) {
+    final database = Provider.of<Database>(context, listen: false);
+    return StreamBuilder(
+      stream: database.jobsStream(),
+      builder: (context, snapshot) {
+        return ListItemBuilder<Job>(
+          snapshot: snapshot,
+          itemBuilder: (context, job) => Dismissible(
+            key: Key('job-${job.id}'),
+            background: Container(color: Colors.red),
+            direction: DismissDirection.endToStart,
+            onDismissed: (direction) => _delete(context, job),
+            child: JobListTile(
+              job: job,
+              onTap: () => JobEntriesPage.show( context, job ,),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
