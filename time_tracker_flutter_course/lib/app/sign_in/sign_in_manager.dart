8277ca358bc2424413c505a088297275085fa379
Fonkam Loïc <fonkamloic@gmail.com>
2020-03-11 18:31:55 +0100

    sign in manager

---
+++ time_tracker_flutter_course/lib/app/sign_in/sign_in_manager.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:flutter/foundation.dart';
+import 'package:time_tracker_flutter_course/services/auth.dart';
+
+class SignInManager {
+  SignInManager({@required this.auth, @required  this.isLoading});
+
+  final ValueNotifier<bool> isLoading;
+  final AuthBase auth;
+  // final StreamController<bool> _isLoadingController = StreamController<bool>();
+
+  // Stream<bool> get isLoadingStream$ => _isLoadingController.stream;
+
+  // void dispose() {
+  //   _isLoadingController.close();
+  // }
+
+  // void _setIsLoading(bool isLoading) => _isLoadingController.add(isLoading);
+
+ Future<User> _signIn(Future<User> Function() signInMethod) async {
+    try {
+      isLoading.value = true;
+      return await signInMethod();
+    } catch (e) {
+      isLoading.value = false;
+      rethrow;
+    }
+  }
+
+  Future<User> signInAnonymously() async => await _signIn(auth.signInAnonymously);
+
+  Future<User> signInWithGoogle() async => await _signIn(auth.signInWithGoogle);
+
+  Future<User> signInWithFacebook() async => await _signIn(auth.signInWithFacebook);
+}
