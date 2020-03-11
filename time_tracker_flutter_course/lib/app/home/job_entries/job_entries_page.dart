e4dc6491327f3418baed0bb1d98f3dde49902c1f
Fonkam Loïc <fonkamloic@gmail.com>
2020-03-11 18:29:59 +0100

    job entries page

---
+++ time_tracker_flutter_course/lib/app/home/job_entries/job_entries_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker_flutter_course/app/home/job_entries/entry_list_item.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/list_items_builder.dart';
+import 'package:time_tracker_flutter_course/app/home/models/entry.dart';
+import 'package:time_tracker_flutter_course/app/home/models/jobs.dart';
+import 'package:time_tracker_flutter_course/common_widgets/platform_exception_alert_dialog.dart';
+import 'package:time_tracker_flutter_course/services/database.dart';
+import 'package:time_tracker_flutter_course/app/home/job_entries/entry_page.dart';
+import 'package:time_tracker_flutter_course/app/home/jobs/edit_job_page.dart';
+
+class JobEntriesPage extends StatelessWidget {
+  const JobEntriesPage({@required this.database, @required this.job});
+
+  final Job job;
+  final Database database;
+
+  static Future<void> show(BuildContext context, Job job) async {
+    final Database database = Provider.of<Database>(context, listen: false);
+
+    await Navigator.of(context).push(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (context) => JobEntriesPage(database: database, job: job),
+      ),
+    );
+  }
+
+  Future<void> _deleteEntry(BuildContext context, Entry entry) async {
+    try {
+      await database.deleteEntry(entry);
+    } on PlatformException catch (e) {
+      PlatformExceptionAlertDialog(title: 'Operation failed', exception: e)
+          .show(context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Job>(
+      stream: database.jobStream(jobId: job.id),
+      builder: (context, snapshot) {
+        final job = snapshot.data;
+        final jobName = job?.name ?? '';
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 2.0,
+            title: Text(jobName),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () =>
+                    EditJobPage.show(context, database: database, job: job),
+                child: Text(
+                  "Edit",
+                  style: TextStyle(fontSize: 18.0, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+          body: _buildContent(context, job),
+          floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () => EntryPage.show(
+                    context: context,
+                    database: database,
+                    job: job,
+                  )),
+        );
+      },
+    );
+  }
+
+  Widget _buildContent(BuildContext context, Job job) {
+    return StreamBuilder<List<Entry>>(
+      stream: database.entriesStream(job: job),
+      builder: (context, snapshot) {
+        return ListItemBuilder<Entry>(
+          snapshot: snapshot,
+          itemBuilder: (context, entry) {
+            return DismissibleEntryListItem(
+                key: Key('entry-${entry.id}'),
+                entry: entry,
+                job: job,
+                onDismissed: () => _deleteEntry(context, entry),
+                onTap: () => EntryPage.show(
+                    context: context,
+                    database: database,
+                    job: job,
+                    entry: entry));
+          },
+        );
+      },
+    );
+  }
+}
