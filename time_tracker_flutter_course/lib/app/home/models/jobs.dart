d6759d997ceaf7aa5fb355899dd78eb0d3dfcf8d
Fonkam Loïc <fonkamloic@gmail.com>
2020-03-20 10:20:01 +0100

    jobs

--- time_tracker_flutter_course/lib/app/home/models/jobs.dart
+++ time_tracker_flutter_course/lib/app/home/models/jobs.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 class Job {
@@ -6,17 +7,16 @@ class Job {
   final String name;
   final int ratePerHour;
 
- factory Job.fromMap(Map<String, dynamic> data, String documentId) {
+  factory Job.fromMap(Map<String, dynamic> data, String documentId) {
     if (data == null) {
       return null;
     }
     final String name = data['name'];
+    if(name == null) {
+      return null;
+    }
     final int ratePerHour = data['ratePerHour'];
-    return Job(
-      id: documentId,
-      name: name,
-      ratePerHour: ratePerHour
-    );
+    return Job(id: documentId, name: name, ratePerHour: ratePerHour);
   }
 
   Map<String, dynamic> toMap() {
@@ -25,4 +25,20 @@ class Job {
       'ratePerHour': ratePerHour,
     };
   }
+
+  @override
+  int get hashCode => hashValues(id, name, ratePerHour);
+
+  @override
+  bool operator ==(other) {
+    if (identical(this, other)) return true;
+    if (runtimeType != other.runtimeType) return false;
+    final Job otherJob = other;
+    return id == otherJob.id &&
+        name == otherJob.name &&
+        ratePerHour == otherJob.ratePerHour;
+  }
+
+  @override
+  String toString() => 'id: $id, name: $name, ratePerHour: $ratePerHour';
 }
